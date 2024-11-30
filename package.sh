350aa214ba33ee6411b02b72a5d625712ec2999f
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-12-01 00:08:58 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -15,3 +15,24 @@ git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-u
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+rm -f feeds/luci/applications/luci-app-ttyd/luasrc/view/terminal/terminal.htm
+wget -P feeds/luci/applications/luci-app-ttyd/luasrc/view/terminal https://xiaomeng9597.github.io/terminal.htm
+
+#集成CPU性能跑分脚本
+cp -a $GITHUB_WORKSPACE/configfiles/coremark/* package/base-files/files/bin/
+chmod 755 package/base-files/files/bin/coremark
+chmod 755 package/base-files/files/bin/coremark.sh
+
+
+# 加入nsy_g68-plus初始化网络配置脚本
+cp -f $GITHUB_WORKSPACE/configfiles/swconfig_install package/base-files/files/etc/init.d/swconfig_install
+chmod 755 package/base-files/files/etc/init.d/swconfig_install
+
+# 电工大佬的rtl8367b驱动资源包，暂时使用这样替换
+wget https://github.com/xiaomeng9597/files/releases/download/files/rtl8367b.tar.gz
+tar -xvf rtl8367b.tar.gz
+
+# openwrt主线rtl8367b驱动资源包，暂时使用这样替换
+# wget https://github.com/xiaomeng9597/files/releases/download/files/rtl8367b-openwrt.tar.gz
+# tar -xvf rtl8367b-openwrt.tar.gz
